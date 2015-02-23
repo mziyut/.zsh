edda0c2440c33084e68e542aac6b7170cef3057c
Yuta Mizui <y.mizui@icloud.com>
2015-02-24 02:00:43 +0900

    add laravel command

--- .zshrc.php
+++ .zshrc.php
@@ -4,9 +4,16 @@
 #                                         Modified : 2014/07/23
 #######################################################################
 
+# compoer
+alias laravel="~/.composer/vendor/bin/laravel"
+
+# laravel
+alias homestead="~/.composer/vendor/bin/homestead"
+
 # cakephp  
 alias cakephp="git clone https://github.com/cakephp/cakephp.git"
 alias debugkit="git clone https://github.com/cakephp/debug_kit.git"
 alias boostcake="git clone https://github.com/slywalker/cakephp-plugin-boost_cake.git"
 
 alias cake="./app/Console/cake"
+
