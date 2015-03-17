6ab595fe739bbae2adf41086c85bed13fd0cef6d
Yuta Mizui <y.mizui@icloud.com>
2015-03-17 22:20:12 +0900

    add phpunit

--- .zshrc.php
+++ .zshrc.php
@@ -10,7 +10,10 @@ alias laravel="~/.composer/vendor/bin/laravel"
 # laravel
 alias homestead="~/.composer/vendor/bin/homestead"
 
-# cakephp  
+# phpunit
+alias phpunit="~/.composer/vendor/bin/phpunit"
+
+# cakephp
 alias cakephp="git clone https://github.com/cakephp/cakephp.git"
 alias debugkit="git clone https://github.com/cakephp/debug_kit.git"
 alias boostcake="git clone https://github.com/slywalker/cakephp-plugin-boost_cake.git"
