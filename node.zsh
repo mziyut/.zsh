082a278451c6dbc572c4e831c746019f2ae3f1a2
Yuta Mizui <y.mizui@icloud.com>
2015-04-24 02:33:07 +0900

    rename files

---
+++ node.zsh
@@ -0,0 +1,11 @@
+#############################################
+#
+# Node .zsh file
+####
+
+#################
+# hokaccha/nodebrew
+#
+# https://github.com/hokaccha/nodebrew
+####
+export PATH=$HOME/.nodebrew/current/bin:$PATH
---
+++ zshrc.zsh
@@ -0,0 +1,22 @@
+# homebrew PATH
+export PATH="/usr/local/bin:$PATH"
+
+# include files
+[ -f ~/.zsh/option.zsh ] && source ~/.zsh/option.zsh
+[ -f ~/.zsh/compensation.zsh ] && source ~/.zsh/compensation.zsh
+[ -f ~/.zsh/history.zsh ] && source ~/.zsh/history.zsh
+[ -f ~/.zsh/color.zsh ] && source ~/.zsh/color.zsh
+[ -f ~/.zsh/alias.zsh ] && source ~/.zsh/alias.zsh
+[ -f ~/.zsh/php.zsh ] && source ~/.zsh/php.zsh
+[ -f ~/.zsh/ruby.zsh ] && source ~/.zsh/ruby.zsh
+[ -f ~/.zsh/java.zsh ] && source ~/.zsh/java.zsh
+[ -f ~/.zsh/personal.zsh ] && source ~/.zsh/personal.zsh
+[ -f ~/.zsh/docker.zsh ] && source ~/.zsh/docker.zsh
+[ -f ~/.zsh/node.zsh ] && source ~/.zsh/node.zsh
+[ -f ~/.zsh/heroku.zsh ] && source ~/.zsh/heroku.zsh
+
+case "${OSTYPE}" in
+	darwin*)
+		[ -f ~/.zsh/mac.zsh ] && source ~/.zsh/mac.zsh
+	;;
+esac
